1694726dc336430f0424f033c89ddd5f54d8e12f
sushen <sushenbiswasaga@gmail.com>
2018-07-30 11:22:58 +0600

    The file is upload for first testing.

---
+++ Pattern 2.py
@@ -0,0 +1,26 @@
+import turtle
+
+def draw_square(some_turtle):
+    for i in range(1,6):
+        some_turtle.forward(50)
+        some_turtle.right(10)
+
+def draw_art():
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+#creat the turtle brade - draw a square
+    brad = turtle.Turtle()
+
+    brad.shape("turtle")
+    brad.color("White")
+    brad.speed(19)
+
+    for i in range(1,37):
+        draw_square(brad)
+        brad.right(50)
+
+
+    window.exitonclick()
+
+draw_art()
